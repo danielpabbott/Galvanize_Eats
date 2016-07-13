33e730d92c5b7e2d0ca9cec566de38fb02bf5323
Daniel Abbott <danielpabbott@gmail.com>
2016-07-13 09:39:36 -0600

    add books view

--- migrations/20160712151822_book.js
+++ migrations/20160712151822_book.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('book', function(table) {
+        table.increments();
+        table.string('title');
+        table.string('genre');
+        table.text('description');
+        table.text('cover_url');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('book');
 };
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/books', function(req, res, next) {
+  return knex('book').select().then(function(book) {
+    res.render('books', {book: book})
+  })
+})
+
 module.exports = router;
